e9c15651903cce59859f5a66486cf8de6ebf0ba0
赵林林 <linlin.zhao@ximalaya.com>
2020-08-27 17:29:03 +0800

    文件改到md5

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var postcss = require('postcss');
-var UUID = require('uuidjs');
 var valueParse = require('postcss-value-parser')
+var fs= require('fs');
+ 
+var crypto = require('crypto');
 
 module.exports = postcss.plugin('postcss-scoped-keyframes-for-wxmini-plugin', function() {
   return function(root) {
-    let uid = UUID.generate();
+    let {file} = root.source.input
+    file= file.replace(/\.css/,'.scss')
+    const md5 = crypto.createHash('md5').update(fs.readFileSync(file), 'utf8').digest('hex');
     let cache = new Set()
     root.walk(function(node) {
       // 选收集 与 加uuid ,
       const {prop,name,type} = node
       if(type=='atrule' && /keyframes/i.test(name)){
         cache.add(node.params)
-        node.params = `${node.params}-${uid}`
+        node.params = `${node.params}-${md5}`
       }
     });
 
@@ -20,7 +24,7 @@ module.exports = postcss.plugin('postcss-scoped-keyframes-for-wxmini-plugin', fu
       if(type=='decl' && prop=='animation'){
           node.value=valueParse(node.value).walk((n)=>{
             if(n.type=='word' && cache.has(n.value)){
-              n.value=`${n.value}-${uid}`
+              n.value=`${n.value}-${md5}`
             }
           }).toString()
       }
